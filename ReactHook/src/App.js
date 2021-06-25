146a165400c2a86cd8c519ab2e90c1c3a86d4677
yashkul <78043029+yashkul@users.noreply.github.com>
2021-06-25 15:17:12 +0530

    Add files via upload

---
+++ ReactHook/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import ResourceExample from "./ResourceExample";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ResourceExample />
+    </div>
+  );
+}
